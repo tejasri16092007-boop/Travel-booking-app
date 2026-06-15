fda8528473ba06e0a1a4d78c830cb9d10001d667
tejasri16092007-boop <tejasri16092007@gmail.com>
2026-06-15 20:26:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import login
+import booking  # Indha line-ai marakkama add pannunga
 
 st.sidebar.title("Navigation")
-menu = ["Home", "Login"]
+menu = ["Home", "Login", "Book Tickets"] # "Book Tickets"-ai menu-la add pannum
 choice = st.sidebar.selectbox("Menu", menu)
 
 if choice == "Home":
@@ -11,3 +12,6 @@ if choice == "Home":
 
 elif choice == "Login":
     login.show_login()
+
+elif choice == "Book Tickets":
+    booking.show_booking() # Booking feature-ai load pannum
